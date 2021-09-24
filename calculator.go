b90a8eddc0db360dd4836d58c7c478dcb15b69c3
maknetaRo <makneta@gmail.com>
2021-09-24 12:14:27 +0200

    go calculator

---
+++ calculator.go
@@ -0,0 +1,17 @@
+package calculator
+
+func Add(a, b float64) float64 {
+	return a + b
+}
+
+func Subtract(a, b float64) float64 {
+	return a - b
+}
+
+func Multiply(a, b float64) float64 {
+	return a * b
+}
+
+func Division(a, b float64) float64 {
+	return a / b
+}
